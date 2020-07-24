a682d6338be4335f47f83fbb66b0534f3d9d7d32
Tulsi Das <tulsi.das@joonko.io>
2020-07-24 16:25:55 +0200

    loading url without exposing url to client

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const request = require('request');
+
+const app = express();
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+
+app.set('port', 9000);
+
+
+
+
+app.get('/', async (req, res)=>{
+    request('https://www.music-knowhow.de/', function (error, response, body) {
+  console.error('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  if(response && response.statusCode === 200){
+    res.json({success: true, body: body});
+  }else {
+    res.json({success: false, error: error});
+  }
+ 
+});
+
+    
+})
+app.listen(app.get('port'), () => {
+    console.log('App is running on http://localhost:%d in %s mode', app.get('port'), app.get('env'));
+})
